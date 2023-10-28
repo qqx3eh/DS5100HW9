90c9350476d808a8dc783229ee515ced84bf2761
qqx3eh <142696321+qqx3eh@users.noreply.github.com>
2023-10-28 10:32:25 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='hw9',
+      url='http://github.com/qqxedh/DS5100HW9/hw9',
+      author='Hithesh Yedlapati',
+      author_email='qqx3eh@virginia.edu',
+      license='MIT',
+      packages=['hw9'])
